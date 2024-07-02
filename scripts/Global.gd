5ccf337a5c7a30b23dd98dc58461a54e7cd8f7d7
SanjyASZ <sanjypro@yahoo.com>
2024-07-03 00:08:00 +0200

    Déplacement amélioré dans la grid de tous les robots

--- scripts/Global.gd
+++ scripts/Global.gd
@@ -6,6 +6,12 @@ var blue_moving = false
 var green_moving = false
 var yellow_moving = false
 
+# Vector indication position robots
+var red_position = Vector2.ZERO
+var blue_position = Vector2.ZERO
+var green_position = Vector2.ZERO
+var yellow_position = Vector2.ZERO
+
 # Position indication of moving robots colliding with static Robots
 var static_robot_position = Vector2.ZERO
 var moving_robot_position = Vector2.ZERO
@@ -14,3 +20,5 @@ var moving_robot_position = Vector2.ZERO
 var moving_collide = false
 var static_collide= false
 
+# Structure to store all combination and find the solution
+var noeud_rac = []
